7fcfa786449add8f20516898ca2759c38e087f5f
melizh <mehld26@gmail.com>
2023-01-03 08:50:12 -0800

    finished plot5

--- plot5.R
+++ plot5.R
@@ -1,2 +1,25 @@
 # How have emissions from motor vehicle sources changed from 1999–2008 
 # in Baltimore City?
+
+rm(list=ls())
+
+scc <- readRDS("./exdata_data_NEI_data/Source_Classification_Code.rds")
+pm25data <- readRDS("./exdata_data_NEI_data/summarySCC_PM25.rds")
+
+# find rows of scc that contain coal-related information
+vehRows <- grep("vehicle",scc$SCC.Level.Two,ignore.case=TRUE)
+vehSensors <- scc$SCC[vehRows]
+
+baltcity <- subset(pm25data,pm25data$fips=="24510")
+
+pm25veh <- baltcity[baltcity$SCC %in% vehSensors,]
+years <- unique(pm25veh$year)
+totyearpol <- tapply(pm25veh$Emissions,pm25veh$year,sum)
+
+# make a plot
+png(filename='plot5.png')
+plot(years,totyearpol,type='l',
+     xlab="Year",
+     ylab="Total PM2.5 Emissions from Vehicles in Baltimore City")
+points(years,totyearpol)
+dev.off()
